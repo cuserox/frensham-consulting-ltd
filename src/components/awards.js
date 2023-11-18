0115498593bca039859156ab7aff396b250abcf0
Celia Usero Navarro <c.usero.navarro@gmail.com>
2023-11-18 11:59:42 +0000

    feat: tweak awards

--- src/components/awards.js
+++ src/components/awards.js
@@ -24,27 +24,33 @@ const Awards = () => {
     <div className="container mx-auto">
       <div className="flex flex-row md:items-center items-start self-auto gap-5 py-6">
         <div className="flex flex-row items-center self-auto md:gap-5 gap-2">
-          <p className="font-display text-display-xs italic opacity-50 vertical-rl -rotate-180">
+          <p className="font-display text-display-xs opacity-50 vertical-rl -rotate-180">
             Certifications
           </p>
           <hr className="md:w-16 w-6 text-white opacity-50"></hr>
         </div>
-        <div className="lg:flex lg:flex-row grid md:grid-cols-2 grid-cols-1 grow xl:gap-16  md:gap-x-10 md:gap-y-8 gap-6">
-          <div className="flex flex-row gap-2 items-center grow">
+        <div className="lg:flex lg:flex-row grid md:grid-cols-2 grid-cols-1 grow gap-40">
+          <div className="flex flex-col items-center justify-center">
             <img
               src={Chartered}
               width={360}
               height={200}
               alt={"chartered engineer"}
             />
+            <p className="text-body-sm font-normal tracking-wider pt-2">
+              {"Chartered member of The Institution of Civil Engineers"}
+            </p>
           </div>
-          <div className="flex flex-row gap-2 items-center grow">
+          <div className="flex flex-col items-center justify-center">
             <img
               src={ICE}
-              width={360}
-              height={200}
+              width={250}
+              height={140}
               alt={"institution of civil engineers"}
             />
+            <p className="text-body-sm font-normal tracking-wider pt-2">
+              {"Registered with the Engineering Council"}
+            </p>
           </div>
         </div>
         {/* {data.allAwardsJson.nodes.map((node) => (
